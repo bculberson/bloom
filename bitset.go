25749c15bf21a0fbdede07fdb0ee1564dd5bb436
bculberson <brad.culberson@sendgrid.com>
2017-01-18 20:05:09 -0700

    Allow for concurrent bitset

--- bitset.go
+++ bitset.go
@@ -8,25 +8,11 @@ type BitSet struct {
 	bitSet *bitset.BitSet
 }
 
-func NewBitSet() *BitSet {
-	return &BitSet{}
+func NewBitSet(m uint) *BitSet {
+	return &BitSet{bitSet: bitset.New(m)}
 }
 
 func (b *BitSet) Set(offsets []uint) error {
-	maxOffset := uint(0)
-	for _, offset := range offsets {
-		if offset > maxOffset {
-			maxOffset = offset
-		}
-	}
-	if b.bitSet == nil {
-		b.bitSet = bitset.New(maxOffset)
-	}
-	if maxOffset > b.bitSet.Len() {
-		previousBitSet := b.bitSet
-		b.bitSet = bitset.New(maxOffset)
-		b.bitSet.InPlaceUnion(previousBitSet)
-	}
 	for _, offset := range offsets {
 		b.bitSet.Set(offset)
 	}
@@ -34,11 +20,8 @@ func (b *BitSet) Set(offsets []uint) error {
 }
 
 func (b *BitSet) Test(offsets []uint) (bool, error) {
-	if b.bitSet == nil {
-		return false, nil
-	}
 	for _, offset := range offsets {
-		if offset > b.bitSet.Len() || !b.bitSet.Test(offset) {
+		if !b.bitSet.Test(offset) {
 			return false, nil
 		}
 	}
